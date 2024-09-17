32dcf2f8fe57de2b0fbd4549a4be02443379989b
Gilson Carmo <gilsoncarmo@windsoft.com.br>
2024-09-17 12:05:02 -0300

    Implementado tamanho mínimo para envio de arquivo 300 KB

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const path = require('path');
 require('dotenv').config(); // Carregar variáveis do arquivo .env
 
 // Caminho para a pasta de gravações
-const gravaçõesPath = process.env.GRAVACOES_PATH;
+const gravacoesPath = process.env.GRAVACOES_PATH;
 const webhookUrl = process.env.WEBHOOK_URL;
 const maxFileSize = 25 * 1024 * 1024; // 25 MB
+const minFileSize = 300 * 1024; // 300 KB
 
 // Função para fazer upload do arquivo
 const uploadFile = async (filePath) => {
@@ -18,6 +19,9 @@ const uploadFile = async (filePath) => {
     // Verificar o tamanho do arquivo
     if (fileSize > maxFileSize) {
       console.log(`Arquivo ${filePath} excede o tamanho máximo de 25 MB. Não será enviado.`);
+      return;
+    } else if (fileSize < minFileSize) {
+      console.log(`Arquivo ${filePath} é menor que o tamanho mínimo de 300 KB. Não será enviado.`);
       return;
     }
   
@@ -34,12 +38,12 @@ const uploadFile = async (filePath) => {
 };
 
 // Iniciar monitoramento da pasta de gravações e subpastas
-const watcher = chokidar.watch(gravaçõesPath, {
+const watcher = chokidar.watch(gravacoesPath, {
   persistent: true,
   ignoreInitial: true, // Ignora os arquivos que já existem na inicialização
   awaitWriteFinish: {
-    stabilityThreshold: 2000,
-    pollInterval: 100,
+    stabilityThreshold: 5000, // Tempo em milissegundos para considerar que a gravação foi concluída
+    pollInterval: 100, // Intervalo de verificação em milissegundos
   },
 });
 
